c63b588f0be9deab063f4c63cbd787642d83be33
Benaya no”git config --global user.name Benaya <0544354125m@gmail.com>
2021-05-31 14:18:56 +0300

    all imported filse from old project to new project

--- app/src/main/java/com/example/daydayweather/MainActivity.kt
+++ app/src/main/java/com/example/daydayweather/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import androidx.fragment.app.commit
-import com.example.daydayweather.view.MapsFragment
+import com.example.daydayweather.view.ViewPagerFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.commit {
-            replace(R.id.fragmentContainer, MapsFragment())
+            replace(R.id.fragmentContainer, ViewPagerFragment())
         }
 
     }
